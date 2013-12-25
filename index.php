18be28eb08e7bfbc8c823e61f451c1ae9841b84e
Dilys Sun <optanovo@gmail.com>
2013-12-25 03:22:54 -0800

    add code snippet that gets and logs params

--- index.php
+++ index.php
@@ -70,6 +70,18 @@
 		          console.log( "Request Failed: " + err );
 		          alert('404 something went wrong. Please contact Dilys');
 		        });
+			      
+			function getQueryVariable(variable) { //jquery url
+			    var query = window.location.search.substring(1);
+			    var vars = query.split('&');
+			    for (var i = 0; i < vars.length; i++) {
+			        var pair = vars[i].split('=');
+			        if (decodeURIComponent(pair[0]) == variable) {
+			            return decodeURIComponent(pair[1]);
+			        }
+			    }
+			    console.log('Query variable %s not found', variable);
+			}
 
 			var profileParam = getQueryVariable("profile");
 			console.log(profileParam);
